66a9d87381f9facf0de10c2a9f9d8a8cf0ee5db8
Anucet Murhula <anicetmurhula20@gmail.com>
2022-11-06 07:39:11 +0200

    Add validations rules to project

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,6 @@
 class Image < ApplicationRecord
     has_one_attached :image
+    validates :image, presence: true
+    validates :title, presence: true
+    validates :description, presence: true
 end
